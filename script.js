85cfa415757404d7cfa472d2e308f93f9b3739b1
Hea092024 <Hea092024@gmail.com>
2024-11-14 12:36:47 +0100

    .

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const button = document.querySelector("#button");
 const input = document.querySelector("#input");
 const firstText = document.querySelector("#first-p");
-const secondText = document.querySelector("#second-p");
+const secondText = document.querySelector("#seconhd-p");
 
 // function inputText() {
 //   firstText.textContent = input.value;
 // }
 //inspect tool
 function inspect(event) {
-  console.log(event);
+  console.log(event.target.id);
 }
-function addPTags(tags, navn) {
+function addPTags(tags, id, navn) {
   let node = document.createElement(tags);
   let parent = document.querySelector("div");
   node.textContent = navn;
+  node.id = id;
+
   parent.appendChild(node);
 }
 
-// const changeText = (event) => {
-//   if (event.target.nodeName === "P" && input.value !== "") {
-//     let temp = document.getElementById(event.target.id);
-//     temp.textContent = input.value;
-//   }
-// };
-
-button.addEventListener("click", inputText);
-document.addEventListener("click", changeText);
+const changeText = (event) => {
+  if (event.target.nodeName === "P" && input.value !== "") {
+    let temp = document.getElementById(event.target.id);
+    temp.textContent = input.value;
+  }
+};
+()=>{}
+document.addEventListener("click",changeText);
