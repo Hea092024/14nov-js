dc3aaca2913901ceb16739607947bd95a5e48fa2
Hea092024 <Hea092024@gmail.com>
2024-11-14 09:41:04 +0100

    .

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const button = document.querySelector("button")
+const button = document.querySelector("button");
+const input = document.querySelector("#input");
+const firsText = document.querySelector("#first-p");
+const secondText = document.querySelector("#second-p");
+
+function inputText(){
+    firsText.innerHTML=input.value;
+}
+button.addEventListener("click",inputText);
